403dcc37e2de8248b97d480669aca2c8be6f8968
Elaine Anderson <eparie@bu.edu>
2018-07-06 18:26:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match(/items/)
       search_term = req.params["item_name"]
       if @@items.include?(search_term)
-        "#{Item.price}"
+        resp.write "#{Item.price}"
       else
         resp.write "Item not found"
      resp.status = 400
