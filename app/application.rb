bac54c8556b44aa75d1eb21e911190a6396df8b5
Elaine Anderson <eparie@bu.edu>
2018-07-06 18:33:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
       search_term = req.params["item_name"]
       if @@items.include?(search_term)
         resp.write "#{search_term.price}"
-      else
+      elsif !@@items.include?(search_term)
         resp.write "Item not found"
      resp.status = 400
     end
