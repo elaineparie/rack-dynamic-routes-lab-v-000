9fe26d38bd2963f0c7c26d412758c651f455b86a
Elaine Anderson <eparie@bu.edu>
2018-07-06 19:14:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     req = Rack::Request.new(env)
     if req.path.match(/items/)
       search_term = req.params["item_name"]
-
+      binding.pry
       if @@items.include?(search_term)
         resp.write "#{Item.price}"
       else
