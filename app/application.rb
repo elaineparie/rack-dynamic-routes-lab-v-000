4d10bb54777320eb1ca178290dbf00da4a10d165
Elaine Anderson <eparie@bu.edu>
2018-07-06 19:19:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,6 @@ class Application
     req = Rack::Request.new(env)
     if req.path.match(/items/)
       search_term = req.path.split("items/")[1]
-      binding.pry
       if @@items.include?(search_term)
         resp.write "#{Item.price}"
       else
